9b4252529af8f262b2003a3eaef96639bbcc7f92
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-10-08 12:03:40 +0200

    about me card

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,9 +1,10 @@
 
 function AboutMe({profileImg, aboutMeText}) {
   return(
-    <div className="aboutme-container">
-      <div className="profile-picture">
+    <div className="aboutme-card">
+      <div className="profile">
         <img src={profileImg} alt="profile picture" />
+        <h3 className="profile-title">Frontend<br/>Web Developer</h3>
       </div>
       <div className="aboutme-content">
         <h2>About Me</h2>
